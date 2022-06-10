ad3c2745e87970f4e84405e5b2629c5543dbd6b3
kirraObj <c@skymc.me>
2022-06-10 17:55:23 +0800

    test: 测试
    这是一个对 git commit template 的测试 commit

--- KirraDungeonClient/src/main/kotlin/net/sakuragame/eternal/kirradungeon/client/compat/dragoncore/screen/DungeonCard.kt
+++ KirraDungeonClient/src/main/kotlin/net/sakuragame/eternal/kirradungeon/client/compat/dragoncore/screen/DungeonCard.kt
@@ -10,11 +10,7 @@ import net.sakuragame.eternal.kirradungeon.client.compat.dragoncore.DungeonAPI.P
 import net.sakuragame.eternal.kirradungeon.client.compat.dragoncore.data.screen.DungeonScreen
 import net.sakuragame.eternal.kirradungeon.client.compat.dragoncore.data.screen.DungeonSubScreen
 import net.sakuragame.eternal.kirradungeon.client.compat.dragoncore.getRealm
-import net.sakuragame.eternal.kirradungeon.client.zone.Zone
-import net.sakuragame.eternal.kirradungeon.client.zone.util.getFeeJoinCounts
-import net.sakuragame.eternal.kirradungeon.client.zone.util.getFeeMaxJoinCounts
 import org.bukkit.entity.Player
-import taboolib.module.chat.colored
 
 
 /**
@@ -27,46 +23,50 @@ object DungeonCard : IScreen {
 
     override fun build2Screen(screen: DungeonScreen, subScreen: DungeonSubScreen, player: Player): ScreenUI {
         return ScreenUI(screenId).apply {
-            addComponent(TextureComp("card_title")
-                .setText(subScreen.name)
-                .setExtend("desc_title")
+            addComponent(
+                TextureComp("card_title")
+                    .setText(subScreen.name)
+                    .setExtend("desc_title")
             )
-            addComponent(TextureComp("card_img", subScreen.description.bgPath)
-                .setExtend("desc_img")
+            addComponent(
+                TextureComp("card_img", subScreen.description.bgPath)
+                    .setExtend("desc_img")
             )
-            val newList = arrayListOf<String>().also {
+            val description = arrayListOf<String>().also {
                 it.addAll(subScreen.description.text)
-                it.add("")
-                it.add(getJoinCountsData(player, subScreen.teleportData) ?: "")
             }
-            addComponent(LabelComp("card_contents", newList)
-                .setExtend("desc_contents")
+            addComponent(
+                LabelComp("card_contents", description)
+                    .setExtend("desc_contents")
             )
             if (!subScreen.isSingle) {
-                addComponent(TextureComp("single_button", "ui/dungeon/button/single.png")
-                    .setXY("body.x + 479", "body.y + 279")
-                    .setWidth("80")
-                    .setHeight("44")
-                    .addAction(ActionType.Left_Click, "single_button.texture = 'ui/dungeon/button/single_press.png';")
-                    .addAction(ActionType.Left_Release, "single_button.texture = 'ui/dungeon/button/single.png';")
-                    .addAction(ActionType.Left_Click, DungeonAPI.getPluginParams(type = JOIN))
+                addComponent(
+                    TextureComp("single_button", "ui/dungeon/button/single.png")
+                        .setXY("body.x + 479", "body.y + 279")
+                        .setWidth("80")
+                        .setHeight("44")
+                        .addAction(ActionType.Left_Click, "single_button.texture = 'ui/dungeon/button/single_press.png';")
+                        .addAction(ActionType.Left_Release, "single_button.texture = 'ui/dungeon/button/single.png';")
+                        .addAction(ActionType.Left_Click, DungeonAPI.getPluginParams(type = JOIN))
                 )
-                addComponent(TextureComp("team_button", "ui/dungeon/button/team.png")
-                    .setXY("body.x + 397", "body.y + 279")
-                    .setWidth("80")
-                    .setHeight("44")
-                    .addAction(ActionType.Left_Click, "team_button.texture = 'ui/dungeon/button/team_press.png';")
-                    .addAction(ActionType.Left_Release, " team_button.texture = 'ui/dungeon/button/team.png';")
-                    .addAction(ActionType.Left_Click, DungeonAPI.getPluginParams(type = JOIN))
+                addComponent(
+                    TextureComp("team_button", "ui/dungeon/button/team.png")
+                        .setXY("body.x + 397", "body.y + 279")
+                        .setWidth("80")
+                        .setHeight("44")
+                        .addAction(ActionType.Left_Click, "team_button.texture = 'ui/dungeon/button/team_press.png';")
+                        .addAction(ActionType.Left_Release, " team_button.texture = 'ui/dungeon/button/team.png';")
+                        .addAction(ActionType.Left_Click, DungeonAPI.getPluginParams(type = JOIN))
                 )
             } else {
-                addComponent(TextureComp("join_button", "ui/dungeon/button/join.png")
-                    .setXY("body.x + 397", "body.y + 279")
-                    .setWidth("163")
-                    .setHeight("44")
-                    .addAction(ActionType.Left_Click, "join_button.texture = 'ui/dungeon/button/join_press.png';")
-                    .addAction(ActionType.Left_Release, "join_button.texture = 'ui/dungeon/button/join.png';")
-                    .addAction(ActionType.Left_Click, DungeonAPI.getPluginParams(type = JOIN))
+                addComponent(
+                    TextureComp("join_button", "ui/dungeon/button/join.png")
+                        .setXY("body.x + 397", "body.y + 279")
+                        .setWidth("163")
+                        .setHeight("44")
+                        .addAction(ActionType.Left_Click, "join_button.texture = 'ui/dungeon/button/join_press.png';")
+                        .addAction(ActionType.Left_Release, "join_button.texture = 'ui/dungeon/button/join.png';")
+                        .addAction(ActionType.Left_Click, DungeonAPI.getPluginParams(type = JOIN))
                 )
             }
             if (player.hasPermission("admin")) {
@@ -86,33 +86,11 @@ object DungeonCard : IScreen {
     }
 
     private fun ScreenUI.addLockComponent() {
-        addComponent(TextureComp("join_lock", "ui/common/lock_shade.png")
-            .setXY("body.x + 397", "body.y + 279")
-            .setWidth("163")
-            .setHeight("44")
+        addComponent(
+            TextureComp("join_lock", "ui/common/lock_shade.png")
+                .setXY("body.x + 397", "body.y + 279")
+                .setWidth("163")
+                .setHeight("44")
         )
-    }
-
-    private fun getJoinCountsData(player: Player, dungeonId: String): String? {
-        val zone = Zone.getByID(dungeonId) ?: return null
-        val maxCounts = player.getFeeMaxJoinCounts(zone)
-        val currentCounts = player.getFeeJoinCounts(zone)
-        // 无限副本次数.
-        if (currentCounts >= 999 || currentCounts == -1) {
-            return "&6&l副本次数: &a无限/无限".colored()
-        }
-        // 次数为零.
-        if (currentCounts <= 0) {
-            return "&6&l副本次数: &c$currentCounts/0".colored()
-        }
-        // 次数等于最大进入次数.
-        if (currentCounts == maxCounts) {
-            return "&6&l副本次数: &a$currentCounts/$maxCounts".colored()
-        }
-        // 次数不满.
-        if (currentCounts < maxCounts) {
-            return "&6&l副本次数: &e$currentCounts/$maxCounts".colored()
-        }
-        return ""
     }
 }
--- KirraDungeonClient/src/main/kotlin/net/sakuragame/eternal/kirradungeon/client/compat/dragoncore/screen/DungeonRoom.kt
+++ KirraDungeonClient/src/main/kotlin/net/sakuragame/eternal/kirradungeon/client/compat/dragoncore/screen/DungeonRoom.kt
@@ -1,6 +1,5 @@
 package net.sakuragame.eternal.kirradungeon.client.compat.dragoncore.screen
 
-import com.google.common.util.concurrent.Atomics
 import com.taylorswiftcn.megumi.uifactory.generate.type.ActionType
 import com.taylorswiftcn.megumi.uifactory.generate.ui.component.base.TextureComp
 import com.taylorswiftcn.megumi.uifactory.generate.ui.screen.ScreenUI
@@ -9,9 +8,11 @@ import net.sakuragame.eternal.kirradungeon.client.compat.dragoncore.DungeonAPI
 import net.sakuragame.eternal.kirradungeon.client.compat.dragoncore.DungeonAPI.getNumber
 import net.sakuragame.eternal.kirradungeon.client.compat.dragoncore.data.screen.DungeonScreen
 import net.sakuragame.eternal.kirradungeon.client.compat.dragoncore.data.screen.DungeonSubScreen
+import net.sakuragame.eternal.kirradungeon.client.zone.Zone
+import net.sakuragame.eternal.kirradungeon.client.zone.util.getFeeJoinCounts
+import net.sakuragame.eternal.kirradungeon.client.zone.util.getFeeMaxJoinCounts
 import org.bukkit.entity.Player
 import taboolib.module.chat.colored
-import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * 副本房间显示实现层.
@@ -37,54 +38,88 @@ object DungeonRoom : IScreen {
             return
         }
 
-        val name = Atomics.newReference<String>(room.name)
-        val forceLock = AtomicBoolean(room.forceLock)
-
         val profile = player.profile() ?: return
 
+        var name = getName(room, player)
+        var forceLock = room.forceLock
+
         if (profile.number < getNumber(room)) {
-            forceLock.set(true)
-            name.set("&7&o暂未解锁".colored())
+            forceLock = true
+            name = "&7&o暂未解锁"
         }
 
         if (room.frameVisible) {
             val framePath =
                 "(global.dungeon_current_selected == $index) ? 'ui/dungeon/selected.png' : 'ui/dungeon/open.png'"
-            addComponent(TextureComp("room_${index}_frame", framePath)
-                .setExtend("dungeon_${index}")
+            addComponent(
+                TextureComp("room_${index}_frame", framePath)
+                    .setExtend("dungeon_${index}")
             )
-            addComponent(TextureComp("room_${index}_string", "ui/dungeon/string.png")
-                .setExtend("dungeon_${index}")
+            addComponent(
+                TextureComp("room_${index}_string", "ui/dungeon/string.png")
+                    .setExtend("dungeon_${index}")
             )
         }
-        if (forceLock.get()) {
-            addComponent(TextureComp("room_${index}_frame", "ui/dungeon/close.png")
-                .setExtend("dungeon_${index}")
+        if (forceLock) {
+            addComponent(
+                TextureComp("room_${index}_frame", "ui/dungeon/close.png")
+                    .setExtend("dungeon_${index}")
             )
-            addComponent(TextureComp("room_${index}_icon", room.iconPath)
-                .setExtend("dungeon_${index}")
+            addComponent(
+                TextureComp("room_${index}_icon", room.iconPath)
+                    .setExtend("dungeon_${index}")
             )
-            addComponent(TextureComp("room_${index}_string_lock", "ui/dungeon/string_lock.png")
-                .setExtend("dungeon_${index}")
+            addComponent(
+                TextureComp("room_${index}_string_lock", "ui/dungeon/string_lock.png")
+                    .setExtend("dungeon_${index}")
             )
-            addComponent(TextureComp("room_${index}_card", "ui/dungeon/card.png")
-                .setExtend("dungeon_${index}")
+            addComponent(
+                TextureComp("room_${index}_card", "ui/dungeon/card.png")
+                    .setExtend("dungeon_${index}")
             )
         } else {
-            addComponent(TextureComp("room_${index}_icon", room.iconPath)
-                .setExtend("dungeon_${index}")
+            addComponent(
+                TextureComp("room_${index}_icon", room.iconPath)
+                    .setExtend("dungeon_${index}")
             )
-            addComponent(TextureComp("room_${index}_card", "ui/dungeon/card.png")
-                .setExtend("dungeon_${index}")
-                .addAction(ActionType.Left_Click,
-                    DungeonAPI.getPluginParams(toScreenData = "current_selected = $index"))
+            addComponent(
+                TextureComp("room_${index}_card", "ui/dungeon/card.png")
+                    .setExtend("dungeon_${index}")
+                    .addAction(
+                        ActionType.Left_Click,
+                        DungeonAPI.getPluginParams(toScreenData = "current_selected = $index")
+                    )
             )
         }
-        addComponent(TextureComp("room_${index}_name", "0,0,0,0")
-            .setText(name.get())
-            .setXY("dungeon_${index}.x", "dungeon_${index}.y + 73")
-            .setWidth("dungeon_${index}.width")
-            .setHeight("10")
+        addComponent(
+            TextureComp("room_${index}_name", "0,0,0,0")
+                .setText(name.colored())
+                .setXY("dungeon_${index}.x", "dungeon_${index}.y + 73")
+                .setWidth("dungeon_${index}.width")
+                .setHeight("10")
         )
+    }
+
+    private fun getName(room: DungeonSubScreen, player: Player): String {
+        val name = room.name
+        if (room.teleportType != DungeonSubScreen.ScreenTeleportType.DUNGEON) {
+            return name
+        }
+        val joinCountsPair = getJoinCountsData(player, room.teleportData) ?: return name
+        val currentCounts = joinCountsPair.first
+        val maxCounts = joinCountsPair.second
+        return when {
+            currentCounts >= 999 -> "$name &a(无限)"
+            currentCounts <= 0 -> "$name &c(&c${currentCounts}/0)"
+            currentCounts == maxCounts -> "$name &a($currentCounts/$maxCounts)"
+            else -> "$name &e($currentCounts/$maxCounts)"
+        }
+    }
+
+    private fun getJoinCountsData(player: Player, dungeonId: String): Pair<Int, Int>? {
+        val zone = Zone.getByID(dungeonId) ?: return null
+        val maxCounts = player.getFeeMaxJoinCounts(zone)
+        val currentCounts = player.getFeeJoinCounts(zone)
+        return currentCounts to maxCounts
     }
 }
