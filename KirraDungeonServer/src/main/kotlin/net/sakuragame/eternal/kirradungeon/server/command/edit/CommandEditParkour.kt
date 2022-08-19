b19a6bd870bab9166f358d7277e8697e45678327
kirraObj <c@skymc.me>
2022-08-19 18:45:01 +0800

    update.

--- KirraDungeonServer/src/main/kotlin/net/sakuragame/eternal/kirradungeon/server/command/edit/CommandEditParkour.kt
+++ KirraDungeonServer/src/main/kotlin/net/sakuragame/eternal/kirradungeon/server/command/edit/CommandEditParkour.kt
@@ -20,6 +20,7 @@ import taboolib.expansion.createHelper
 import taboolib.module.chat.colored
 import java.util.*
 
+@Suppress("DuplicatedCode")
 @CommandHeader("DungeonEditParkour")
 object CommandEditParkour {
 
@@ -60,25 +61,75 @@ object CommandEditParkour {
 
     @CommandBody
     val setParkourBlock = subCommand {
-        dynamic(commit = "multiplyValue") {
-            dynamic(commit = "yValue") {
-                execute<Player> { player, context, _ ->
-                    val multiplyValue = context.get(1).toDoubleOrNull() ?: return@execute
-                    val yValue = context.get(2).toDoubleOrNull() ?: return@execute
-                    val block = player.getTargetBlock(setOf(Material.AIR), 3)
-                    val entity = (block.world as CraftWorld).getTileEntityAt(block.x, block.y, block.z) as? TileEntitySkull ?: run {
-                        player.sendMessage("&c[System] &7无法转换成 TileEntity 数据.".colored())
-                        return@execute
+        dynamic(commit = "skullId") {
+            dynamic(commit = "multiplyValue") {
+                dynamic(commit = "yValue") {
+                    execute<Player> { player, context, _ ->
+                        val skullId = context.get(1)
+                        val multiplyValue = context.get(2).toDoubleOrNull() ?: return@execute
+                        val yValue = context.get(3).toDoubleOrNull() ?: return@execute
+                        val block = player.getTargetBlock(setOf(Material.AIR), 3)
+                        val entity = (block.world as CraftWorld).getTileEntityAt(block.x, block.y, block.z) as? TileEntitySkull ?: run {
+                            player.sendMessage("&c[System] &7无法转换成 TileEntity 数据.".colored())
+                            return@execute
+                        }
+                        entity.gameProfile = GameProfile(UUID.randomUUID(), skullId).apply {
+                            properties.put("textures", Property("", ""))
+                            properties.put("model", Property("model", "model: $skullId"))
+                            properties.put("multiplyValue", Property("$multiplyValue", "$multiplyValue"))
+                            properties.put("yValue", Property("$yValue", "$yValue"))
+                        }
+                        entity.update()
+                        block.state.update()
+                        player.sendMessage("&c[System] &7成功设置.".colored())
                     }
-                    entity.gameProfile = entity.gameProfile ?: GameProfile(UUID.randomUUID(), "").apply {
-                        properties.put("multiplyValue", Property("$multiplyValue", "$multiplyValue"))
-                        properties.put("yValue", Property("$yValue", "$yValue"))
-                    }
-                    entity.update()
-                    block.state.update()
-                    player.sendMessage("&c[System] &7成功设置.".colored())
                 }
             }
+        }
+    }
+
+    @CommandBody
+    val getParkourBlockStatus = subCommand {
+        execute<Player> { player, _, _ ->
+            val block = player.getTargetBlock(setOf(Material.AIR), 3)
+            val entity = (block.world as CraftWorld).getTileEntityAt(block.x, block.y, block.z) as? TileEntitySkull ?: run {
+                player.sendMessage("&c[System] &7无法转换成 TileEntity 数据.".colored())
+                return@execute
+            }
+            val gameProfile = entity.gameProfile
+            if (gameProfile == null) {
+                player.sendMessage("&c[System] &7方块不存在.".colored())
+                return@execute
+            }
+            val multiplyValue = gameProfile.properties.get("multiplyValue")?.firstOrNull()?.value?.toDoubleOrNull()
+            val yValue = gameProfile.properties.get("yValue")?.firstOrNull()?.value?.toDoubleOrNull()
+            player.sendMessage("&c[System] &7MultiplyValue = $multiplyValue, yValue: $yValue".colored())
+        }
+    }
+
+    @CommandBody
+    val resetStatus = subCommand {
+        execute<Player> { player, _, _ ->
+            val block = player.getTargetBlock(setOf(Material.AIR), 3)
+            val entity = (block.world as CraftWorld).getTileEntityAt(block.x, block.y, block.z) as? TileEntitySkull ?: run {
+                player.sendMessage("&c[System] &7无法转换成 TileEntity 数据.".colored())
+                return@execute
+            }
+            val gameProfile = entity.gameProfile
+            if (gameProfile == null) {
+                player.sendMessage("&c[System] &7方块不存在.".colored())
+                return@execute
+            }
+            val multiplyValue = gameProfile.properties.get("multiplyValue")?.firstOrNull()?.value?.toDoubleOrNull()
+            val yValue = gameProfile.properties.get("yValue")?.firstOrNull()?.value?.toDoubleOrNull()
+            val skullId = gameProfile.properties.get("model")?.firstOrNull()?.value?.replace("model: ", "") ?: return@execute
+            entity.gameProfile = GameProfile(UUID.randomUUID(), skullId).apply {
+                properties.put("textures", Property("", ""))
+                properties.put("model", Property("model", "model: $skullId"))
+                properties.put("multiplyValue", Property("$multiplyValue", "$multiplyValue"))
+                properties.put("yValue", Property("$yValue", "$yValue"))
+            }
+            player.sendMessage("&c[System] &7MultiplyValue = $multiplyValue, yValue: $yValue".colored())
         }
     }
 }
